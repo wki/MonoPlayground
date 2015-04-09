dc451338505a301cb0ba660d7986d9f005d872d1
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-04-09 22:12:53 +0200

    First Commit

---
+++ CmdLineApp/AppOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CommandLine;
+using CommandLine.Text;
+
+namespace CmdLineApp
+{
+    public class AppOptions
+    {
+        [Option('v', "verbose", DefaultValue = false, HelpText = "generate more output")]
+        public bool Verbose { get; set; }
+
+        [Option('d', "directory", Required = true, HelpText = "directory to list")]
+        public string Directory { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+                helptext => HelpText.DefaultParsingErrorsHandler(this, helptext)
+            );
+        }
+    }
+}
---
+++ CmdLineApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+//using System.Diagnostics;
+using System.Linq;
+// using Mono.Unix;
+using TwoPS.Processes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdLineApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var options = new AppOptions();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                Console.WriteLine("Hello World!, dir={0}", options.Directory);
+
+                ListDirectory2(options.Directory);
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong: {0}",
+                    String.Join(
+                        "/",
+                        options.LastParserState.Errors
+                        .Select(e => e.BadOption.ShortName + ": " + e.ViolatesRequired)));
+            }
+        }
+
+        public static void ListDirectory2(params string[] args)
+        {
+            var process = new Process("ls", args);
+            var result = Task.Run(() => process.Run()).Result;
+
+            if (result.ExitCode != 0)
+            {
+                Console.WriteLine("Received Error: ***{0}***, exit code: {1}",
+                    result.StandardError, result.ExitCode
+                );
+            }
+            else
+            {
+                Console.WriteLine("Received Output: ***{0}***",
+                    result.StandardOutput
+                );
+            }
+        }
+
+        // using System.Diagnostics:
+//        public static void ListDirectory(string path)
+//        {
+//            var startInfo = new ProcessStartInfo
+//            {
+//                FileName = "ls",
+//                Arguments = path,
+//                CreateNoWindow = true,
+//                RedirectStandardOutput = true,
+//                RedirectStandardError = true,
+//                UseShellExecute = false,
+//            };
+//
+//            var child = Process.Start(startInfo);
+//            var stdout = child.StandardOutput;
+//            var stderr = child.StandardError;
+//
+//            var output = stdout.ReadToEnd();
+//            var error = stderr.ReadToEnd();
+//
+//            child.WaitForExit();
+//
+//            if (child.ExitCode != 0)
+//            {
+//                Console.WriteLine("Received Error: ***{0}***, exit code: {1}",
+//                    error,
+//                    child.ExitCode
+//                );
+//            }
+//            else
+//            {
+//                Console.WriteLine("Received Output: ***{0}***, exit code: {1}",
+//                    output, 
+//                    child.ExitCode
+//                );
+//            }
+//        }
+    }
+}
