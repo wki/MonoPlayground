e8619bf6d07fa183eb308948bf6cf66429053c23
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-04-11 10:41:29 +0200

    added forgotten file

---
+++ AdobeApp.Tests/ApplicationTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using AdobeApp;
+using System;
+
+namespace AdobeApp.Tests
+{
+    [TestFixture]
+    public class ApplicationTest
+    {
+        private Application app;
+
+        [SetUp]
+        public void SetUp()
+        {
+            app = new Application("Foo");
+        }
+
+        [Test]
+        public void AdobeApp_Constructor_SetsName()
+        {
+            // assert
+            Assert.AreEqual("Foo", app.Name);
+        }
+
+        [Test]
+        public void AdobeApp_Invocation_CreatesInvocationObject()
+        {
+            // Arrange
+            var invocation = app.Invocation("some.js");
+
+            // Assert
+            Assert.IsNotNull(invocation);
+        }
+    }
+}
