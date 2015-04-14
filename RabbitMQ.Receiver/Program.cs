2d5c6a6bc2ededef61b0d491f54995e4d4b6f518
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-04-14 20:30:39 +0200

    added a simple RabbitMQ sender and receiver project

---
+++ RabbitMQ.Receiver/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Receiver
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wait for messages");
+            WaitForMessages();
+        }
+
+        public static void WaitForMessages()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new QueueingBasicConsumer(channel);
+                channel.BasicConsume("message", true, consumer);
+
+                while (true)
+                {
+                    var eventArgs = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                    var body = eventArgs.Body;
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine("Received: {0}", message);
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ.Sender/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Sender
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Publishing Message...");
+            Publish(args.Length > 0 ? String.Join(" ", args) : "Hello world!");
+        }
+
+        public static void Publish(string message)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("message", "direct", true);
+                channel.QueueDeclare("message", true, false, false, null);
+                channel.QueueBind("message", "message", "info");
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("message", "info", null, body);
+            }
+        }
+    }
+}
