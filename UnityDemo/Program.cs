bb8acca19e6f6255d742675d63450eba53f4536f
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-04-16 20:54:47 +0200

    added a Unity Container demo

---
+++ UnityDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+
+namespace UnityDemo
+{
+    public interface IService 
+    {
+        void DoSomething();
+    }
+
+    public class MyService : IService
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("MyService implements IService");
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ContainerDemo();
+        }
+
+        static void ContainerDemo()
+        {
+            using (var container = new UnityContainer())
+            {
+                container.RegisterInstance<IService>(new MyService());
+
+                var myService = container.Resolve<IService>();
+                myService.DoSomething();
+            }
+        }
+    }
+}
