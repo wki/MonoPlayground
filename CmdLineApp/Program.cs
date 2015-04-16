e1b5e8319461dc6df9812499e03ee6f89e784f86
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-04-16 21:05:17 +0200

    cleanup

--- CmdLineApp/Program.cs
+++ CmdLineApp/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-//using System.Diagnostics;
 using System.Linq;
-// using Mono.Unix;
 using TwoPS.Processes;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CmdLineApp
@@ -47,43 +44,5 @@ namespace CmdLineApp
                 );
             }
         }
-
-        // using System.Diagnostics:
-//        public static void ListDirectory(string path)
-//        {
-//            var startInfo = new ProcessStartInfo
-//            {
-//                FileName = "ls",
-//                Arguments = path,
-//                CreateNoWindow = true,
-//                RedirectStandardOutput = true,
-//                RedirectStandardError = true,
-//                UseShellExecute = false,
-//            };
-//
-//            var child = Process.Start(startInfo);
-//            var stdout = child.StandardOutput;
-//            var stderr = child.StandardError;
-//
-//            var output = stdout.ReadToEnd();
-//            var error = stderr.ReadToEnd();
-//
-//            child.WaitForExit();
-//
-//            if (child.ExitCode != 0)
-//            {
-//                Console.WriteLine("Received Error: ***{0}***, exit code: {1}",
-//                    error,
-//                    child.ExitCode
-//                );
-//            }
-//            else
-//            {
-//                Console.WriteLine("Received Output: ***{0}***, exit code: {1}",
-//                    output, 
-//                    child.ExitCode
-//                );
-//            }
-//        }
     }
 }
