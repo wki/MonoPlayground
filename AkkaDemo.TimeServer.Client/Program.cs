f8738fa8bb928d9715362b5b6a7cb6fcf15b6ccf
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-06-09 21:03:56 +0200

    added timeserver client -- not wirking yet

--- AkkaDemo.TimeServer.Client/Program.cs
+++ AkkaDemo.TimeServer.Client/Program.cs
@@ -1,15 +1,34 @@
 ﻿using Akka.Actor;
+using AkkaDemo.TimeServer.Actors;
 using System;
 
 namespace AkkaDemo.TimeServer.Client
 {
     class MainClass
     {
+        private static ActorSystem system;
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Time Server client");
 
-            var system = Akka.Actor.ActorSystem.Create("TimeServer");
+            system = Akka.Actor.ActorSystem.Create("TimeClient");
+
+            ReadTime();
+
+            system.AwaitTermination();
+        }
+
+        private static async void ReadTime()
+        {
+            var time = system.ActorSelection("akka.tcp://TimeServer@localhost:9000/user/time");
+
+            var client = system.ActorOf(
+                Props.Create( () => new Tim
+
+            var result = await time.Ask<string>(new TimeServerActor.GetTime());
+
+            Console.WriteLine("Time received: {0}", result);
         }
     }
 }
