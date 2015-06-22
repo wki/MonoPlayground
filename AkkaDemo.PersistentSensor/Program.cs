7d9563405cd3006c0519d789119e9bab189fa0e6
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-06-22 22:36:50 +0200

    expansion of Persistent demo

--- AkkaDemo.PersistentSensor/Program.cs
+++ AkkaDemo.PersistentSensor/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Akka.Actor;
+using System;
 
 namespace AkkaDemo.PersistentSensor
 {
@@ -6,7 +7,21 @@ namespace AkkaDemo.PersistentSensor
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting Actor System");
+
+            var system = Akka.Actor.ActorSystem.Create("Measurement");
+            var sensor1 = system.ActorOf<SensorActor>("sensor1");
+
+            sensor1.Tell(new SensorActor.ProvideMeasurement(42));
+            sensor1.Tell("print");
+            sensor1.Tell("snap");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Stopping Actor System");
+
+            system.Shutdown();
+            system.AwaitTermination();
         }
     }
 }
