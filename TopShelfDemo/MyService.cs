535152f80fcf0711676ac80a9185da76249c126a
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-07-08 20:48:45 +0200

    added topshelf demo

---
+++ TopShelfDemo/MyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Topshelf;
+
+namespace TopShelfDemo
+{
+    public class MyService : ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            Console.WriteLine("Starting MyService");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Console.WriteLine("Stopping MyService");
+            return true;
+        }
+    }
+}
+
