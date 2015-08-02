407e66d813306625dbde6abcba52f71441127b99
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-08-02 07:21:24 +0200

    fixed typo

--- QuartzDemo/Program.cs
+++ QuartzDemo/Program.cs
@@ -21,7 +21,7 @@ namespace QuartzDemo
 
             QuartzAPI.Configure(builder =>
                 {
-                    builder.UseScheduler(scheduler); //This is the scheduler you are using, in this case I'm pulling out the scheduler from my structuremap container
+                    builder.UseScheduler(scheduler);
                     builder.EnableCors();
                 });
             QuartzAPI.Start("http://localhost:9001/");
