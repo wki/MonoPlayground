ac6d8674b754dc77c68709c1b7a0bad26a4d169e
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-09-25 19:43:42 +0200

    expanded gitignore

---
+++ WebApiAkkaDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApiAkkaDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
