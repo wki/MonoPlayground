17f7d5960d5c5208e5ffffe9cece3e6c51258cdc
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-09-30 20:55:08 +0200

    expanded Rx Demo

--- RxDemo/Program.cs
+++ RxDemo/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Reactive;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Collections.Generic;
+// using System.Threading;
 
 namespace RxDemo
 {
@@ -9,15 +12,28 @@ namespace RxDemo
     {
         public static void Main(string[] args)
         {
-            var xs = "Hello World".ToObservable();
+            // HelloWorld();
+            // Test();
+            // Grouping();
+            FloatingWindow();
 
-            xs.Subscribe(x => Console.WriteLine(x));
-
-            Test();
-
+            Console.WriteLine("Done with all.");
             Console.ReadLine();
         }
 
+        // observe characters of a string
+        public static void HelloWorld()
+        {
+            var xs = "Hello World".ToObservable();
+
+            xs.Subscribe(x => Console.WriteLine(x));
+        }
+
+        public static void ExperimentalStuff()
+        {
+        }
+
+        // observe one event occuring
         public static void Test()
         {
             var s = new Subject<int>();
@@ -25,6 +41,57 @@ namespace RxDemo
             {
                 s.OnNext(42);
             }
+        }
+
+        // group input lines by length
+        public static void Grouping()
+        {
+            var source = GetInput().ToObservable(Scheduler.Default);
+            var result = source.GroupBy(s => s.Length);
+
+            result.ForEachAsync(g =>
+                {
+                    Console.WriteLine("New group with Length: {0}", g.Key);
+
+                    g.Subscribe(x => Console.WriteLine("  {0} member of {1}", x, g.Key));
+                }
+            ).Wait();
+        }
+
+        // pack together 3 things into a non overlapping window
+        public static void NonOverlappingWindow()
+        {
+            var source = GetInput().ToObservable(Scheduler.Default);
+            var result = source.Window(3);
+
+            result.ForEachAsync(w =>
+                {
+                    Console.WriteLine("New window created", ;
+                    w.Subscribe(Console.WriteLine);
+                }
+            ).Wait();
+        }
+
+        // pack together 3 things into a floating window
+        public static void FloatingWindow()
+        {
+            var source = GetInput().ToObservable(Scheduler.Default);
+            var result = source.Window(3,1);
+
+            var i = 0;
+            result.ForEachAsync(w =>
+                {
+                    var j = i++;
+                    Console.WriteLine("New window {0} created", j);
+                    w.Subscribe(x => Console.WriteLine("  {0} in {1}", x, j));
+                }
+            ).Wait();
+        }
+
+        public static IEnumerable<string> GetInput()
+        {
+            while (true)
+                yield return Console.ReadLine();
         }
     }
 }
